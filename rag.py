51d25c3a7cb571f8cb3433d1761730c62325965d
Anjolaoluwa Ajayi <anjolaajayi3@gmail.com>
2024-10-17 02:12:17 +0100

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,3 +1,58 @@
+# Required imports
+import json
+import time
+import os
+from sentence_transformers import SentenceTransformer
+from pinecone import Pinecone, ServerlessSpec
+from groq import Groq
+from tqdm.auto import tqdm
+import streamlit as st
+import re
+
+# Constants (hardcoded)
+FILE_PATH = "anjibot_chunks.json"
+BATCH_SIZE = 384
+INDEX_NAME = "groq-llama-3-rag"
+PINECONE_API_KEY = st.secrets["keys"]["PINECONE_API_KEY"]
+GROQ_API_KEY = st.secrets["keys"]["GROQ_API_KEY"]
+DIMS = 768
+encoder = SentenceTransformer('dwzhu/e5-base-4k')
+groq_client = Groq(api_key=GROQ_API_KEY)
+
+with open(FILE_PATH, 'r') as file:
+        data= json.load(file)
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+spec = ServerlessSpec(cloud="aws", region='us-east-1')
+existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+# Check if index already exists; if not, create it
+if INDEX_NAME not in existing_indexes:
+    pc.create_index(INDEX_NAME, dimension=DIMS, metric='cosine', spec=spec)
+
+    # Wait for the index to be initialized
+    while not pc.describe_index(INDEX_NAME).status['ready']:
+        time.sleep(1)
+
+index = pc.Index(INDEX_NAME)
+
+for i in tqdm(range(0, len(data['id']), BATCH_SIZE)):
+    # Find end of batch
+    i_end = min(len(data['id']), i + BATCH_SIZE)
+
+    # Create batch
+    batch = {k: v[i:i_end] for k, v in data.items()}
+
+    # Create embeddings
+    chunks = [f'{x["title"]}: {x["content"]}' for x in batch["metadata"]]
+    embeds = encoder.encode(chunks)
+
+    # Ensure correct length
+    assert len(embeds) == (i_end - i)
+
+    # Upsert to Pinecone
+    to_upsert = list(zip(batch["id"], embeds, batch["metadata"]))
+    index.upsert(vectors=to_upsert)
+
 def extract_course_code(text) -> list[str]:
     pattern = r'\b(?:geds?|stats?|maths?|cosc|seng|itgy)\s*\d{3}\b'
     match = re.findall(pattern, text, re.IGNORECASE)
